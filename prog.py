157323b33834224c7b06c4dc495788516bdf6ef5
wolkoduff <ignatikus@bk.ru>
2022-10-02 14:59:27 +0300

    Доработки от 02.09.2022: обработка сообщения с выбором + try-catch в методе для обработки ошибок

--- prog.py
+++ prog.py
@@ -14,24 +14,37 @@ def clicked():
         num -= 1
 
     res = "Привет {}".format(txt.get())
-    res = res + str(num)
     label.configure(text=res)
     
     txt.configure(state=listState.get(num))
     
     messagebox.showinfo('Держи результат', 'Я же просил не нажимать!!!')
+    messagebox.showwarning('ТЫ уверен?', 'Ты точно не будешь больше писать?')
+    try:
+        flag = messagebox.askyesno('Точно?', 'Ты ведь не будешь больше нажимать, правда?')
+        if (flag):
+            messagebox.showinfo('НЯЯЯЯЯЯ', 'Я проверю и слежу за тобой')
+        else:
+            messagebox.showerror('Редиска', 'Я доверяла тебе! Ты был(-а) мне как брат(сестра), питушара!')
+    except Exception as ex:
+        messagebox.showerror('Ошибка', str(ex))
 
-
-window = Tk()
-listState = {-1 : 'disabled', 0 : 'normal'}
-window.title("Добро пожаловать!")
-label = Label(window, text="Ахах, ЧЫЖЫК", font=("Times New Roman", 24), bg="yellow", fg="blue")
-lbl = Label(window, text="Ахах, ЧВК 'ЧИЖИК'", font=("Times New Roman", 24), bg="yellow", fg="blue")
-label.grid(column=0, row=0)
-lbl.grid(column=0, row=1)
-txt = Entry(window, width=10, state=listState.get(num), show='*')
-txt.grid(column=2, row=0)
-btn = Button(window, text="Не нажимать!", command=clicked)
-btn.grid(column=1, row=0)
-window.geometry('640x480')
-window.mainloop()
+isStart = messagebox.askyesno('Ты уверен?', 'Ты хочешь запустить программу?')
+if (isStart):
+    window = Tk()
+    listState = {-1 : 'disabled', 0 : 'normal'}
+    window.title("Добро пожаловать!")
+    label = Label(window, text="Ахах, ЧЫЖЫК", font=("Times New Roman", 24), bg="yellow", fg="blue")
+    lbl = Label(window, text="Ахах, ЧВК 'ЧИЖИК'", font=("Times New Roman", 24), bg="yellow", fg="blue")
+    label.grid(column=0, row=0)
+    lbl.grid(column=0, row=1)
+    txt = Entry(window, width=10, state=listState.get(num), show='*')
+    txt.grid(column=2, row=0)
+    btn = Button(window, text="Не нажимать!", command=clicked)
+    btn.grid(column=1, row=0)
+    window.geometry('1366x768')
+    window.wm_attributes("-alpha", 0.85)
+    window.resizable(width=False, height=False)
+    window.mainloop()
+else:
+    exit
