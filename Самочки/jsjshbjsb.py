05871f674db7b774f842257ec368ac688d966a03
wolkoduff <ignatikus@bk.ru>
2022-10-02 13:57:22 +0300

    Правка ошибок

--- Самочки/jsjshbjsb.py
+++ Самочки/jsjshbjsb.py
@@ -15,13 +15,13 @@ def clicked():
     txt.configure(state=listState.get(num))
     
 window = Tk ()
-listState = [ -1 : 'disabled', 0 : 'readonly']
+listState = {-1 : 'disabled', 0 : 'readonly'}
 window.title ("Добро пожаловать!")
 label = Label(window, text="Ахах,ЧЫЖЫК", font=("Times New Roman", 24), bg="yellow", fg="blue")
 lbl = Label(window, text="Ахах, ЧВК ЧИЖИК", font=("Times New Roman", 24), bg="yellow", fg="blue")
 label.grid(column=0, row=0)
 lbl.grid(column=0, row=1)
-txt = Entry(window, width=10, state)
+txt = Entry(window, width=10)
 txt.grid(column=2, row=0)
 btn=Button(window, text="Не нажинать!", command=clicked)
 btn.grid(column=1,row=0)
