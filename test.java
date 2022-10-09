bed5e73af243d121b87dd548244f4297fbf54003
wolkoduff <ignatikus@bk.ru>
2022-10-09 14:56:31 +0300

    Дополнительный разбор полётов у ребят + комбобокс с фокусом на текстовом поле

---
+++ test.java
@@ -0,0 +1,25 @@
+class Test {
+    private void print(String str) {
+        System.out.print(str);
+    }
+
+    public static void main(String...args) {
+        System.out.println("Лолололошка"); 
+        print("Паша паштет");
+        print("тет");
+         print("-а-");
+          print("тет");
+        boolean fuck = true;
+        if (fuck) {
+            print("\nНЕЕЕЕТ");
+          print("\nЕЕЕЕЕЕЕЕ")
+        }
+    }
+}
+
+
+/**
+
+print("Лолололошка")
+
+ */
