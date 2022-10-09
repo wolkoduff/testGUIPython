72353b228eb44ad997e895a05e190318733b728e
wolkoduff <ignatikus@bk.ru>
2022-10-09 14:58:49 +0300

    Что не ушло сразу

--- prog.py
+++ prog.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter.ttk import Combobox
 
 num = -1
 
@@ -13,38 +14,31 @@ def clicked():
     else:
         num -= 1
 
-    res = "Привет {}".format(txt.get())
+    str = txt.get()
+    res = "Привет {}".format(str)
+    txt.delete(0, len(str))
     label.configure(text=res)
+    messagebox.showinfo('ОБрати Внимание!!! СДЕЛАНО В ГЕРМАНИИ!!!', 'Ваше значение в комбо-боксе следующее: ' + combo.get())
     
-    txt.configure(state=listState.get(num))
+    #txt.configure(state=listState.get(num))
     
-    messagebox.showinfo('Держи результат', 'Я же просил не нажимать!!!')
-    messagebox.showwarning('ТЫ уверен?', 'Ты точно не будешь больше писать?')
-    try:
-        flag = messagebox.askyesno('Точно?', 'Ты ведь не будешь больше нажимать, правда?')
-        if (flag):
-            messagebox.showinfo('НЯЯЯЯЯЯ', 'Я проверю и слежу за тобой')
-        else:
-            messagebox.showerror('Редиска', 'Я доверяла тебе! Ты был(-а) мне как брат(сестра), питушара!')
-    except Exception as ex:
-        messagebox.showerror('Ошибка', str(ex))
-
-isStart = messagebox.askyesno('Ты уверен?', 'Ты хочешь запустить программу?')
-if (isStart):
-    window = Tk()
-    listState = {-1 : 'disabled', 0 : 'normal'}
-    window.title("Добро пожаловать!")
-    label = Label(window, text="Ахах, ЧЫЖЫК", font=("Times New Roman", 24), bg="yellow", fg="blue")
-    lbl = Label(window, text="Ахах, ЧВК 'ЧИЖИК'", font=("Times New Roman", 24), bg="yellow", fg="blue")
-    label.grid(column=0, row=0)
-    lbl.grid(column=0, row=1)
-    txt = Entry(window, width=10, state=listState.get(num), show='*')
-    txt.grid(column=2, row=0)
-    btn = Button(window, text="Не нажимать!", command=clicked)
-    btn.grid(column=1, row=0)
-    window.geometry('1366x768')
-    window.wm_attributes("-alpha", 0.85)
-    window.resizable(width=False, height=False)
-    window.mainloop()
-else:
-    exit
+window = Tk()
+# listState = {-1 : 'disabled', 0 : 'normal'}
+window.title("Добро пожаловать!")
+label = Label(window, text="Ахах, ЧЫЖЫК", font=("Times New Roman", 24))
+lbl = Label(window, text="Ахах, ЧВК 'ЧИЖИК'", font=("Times New Roman", 24))
+label.grid(column=0, row=0)
+lbl.grid(column=0, row=1)
+txt = Entry(window, width=10, show='*')
+txt.grid(column=2, row=0)
+combo = Combobox(window)
+combo['values'] = (1, 2, 3, 4, 5, 'Тухляк-поляк')
+combo.current(5)
+combo.grid(column=3,row=5)
+btn = Button(window, text="Не нажимать!", command=clicked)
+btn.grid(column=1, row=0)
+btn.focus()
+window.geometry('1366x768')
+# window.wm_attributes("-alpha", 0.85)
+window.resizable(width=false, height=false)
+window.mainloop()
