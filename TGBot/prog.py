d739ba3f573a7f22c19610f5371e0b05fd78c315
wolkoduff <ignatikus@bk.ru>
2022-12-04 15:12:06 +0300

    Телеграм бот конфигурация

---
+++ TGBot/prog.py
@@ -0,0 +1,20 @@
+import telebot
+import config
+
+# Инициализировали бота
+bot = telebot.TeleBot(config.TOKEN)
+
+# Обработка команды /start
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Бодро пожаловать, *{0.last_name}* _{1.first_name}_!\nЯ - *{2.first_name}*, бот."
+    .format(message.from_user, message.from_user, bot.get_me()), parse_mode='markdown')
+
+# Если создаём эхо,т.е. что не отправь, он ответит, тогда пишем
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+# Запускаем бота
+bot.polling(none_stop=True) # не останавливаться
+
