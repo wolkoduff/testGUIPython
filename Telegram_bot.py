ea7d082576e68d02d1ca64cc85c34df471505609
Wolkoduff <igantikus@bk.ru>
2022-12-10 18:22:39 +0300

    рефактор для PEP-8

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -13,37 +13,42 @@
                Заготовки на новый год, что будем проходить и делать, а также, что хотелось бы изучить
 """
 
+
 # Создать файл конфигурации для его импорта.
 # ВАЖНО: правила хорошего тона предусматривают создание файлов конфигурации и настроек, а не тащить всё в одном файле
 # В файле config.py пишем TOKEN = 'токен', куда вместо слова @токен вставляем полученный токен от @BotFather
 
 class config:
-    TOKEN = '' # token from @BotFather
+    TOKEN = ''  # token from @BotFather
+
 
 import telebot
+
 bot = telebot.TeleBot(config.TOKEN)
+
 
 # Если создаём эхо,т.е. что не отправь, он ответит, тогда пишем
 @bot.message_handler(content_types=['text'])
 def echo(message):
     bot.send_message(message.chat.id, message.text)
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Добро пожаловать, _{0.first_name}_!\nЯ - *{1.first_name}*, бот ".format(
         message.from_user, bot.get_me()), parse_mode='markdown', )
 
+
 @bot.message_handler(commands=['Стикер'])
 def send_sticker(message):
     stiker = open('stikers/')
 
-    #keyboard
+    # keyboard
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = telebot.types.KeyboardButton("Первая кнопка")
     item2 = telebot.types.KeyboardButton("Вторая кнопка")
     markup.add(item1, item2)
 
 
-
 # Запускаем бота
-bot.polling(none_stop=True) # не останавливаться
+bot.polling(none_stop=True)  # не останавливаться
