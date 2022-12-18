4892ea4fdccbed2b81faa85379c56e5d163ebac6
wolkoduff <ignatikus@bk.ru>
2022-12-18 15:32:51 +0300

    Телеграм-бот обновки

--- TGBot/prog.py
+++ TGBot/prog.py
@@ -1,17 +1,27 @@
 import telebot
-import config
+from config import *
 import os
 import random
 
+from telebot.types import *
+
 # Инициализировали бота
-bot = telebot.TeleBot(config.TOKEN)
+bot = telebot.TeleBot(TOKEN)
 
 
 # Обработка команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Бодро пожаловать, *{0.last_name}* _{1.first_name}_!\nЯ - *{2.first_name}*, бот."
+    # Клавиатура
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    for text in REPLY_LIST:
+        markup.add(KeyboardButton(text))
+
+    chat_id = message.chat.id
+
+    bot.send_message(chat_id, "Бодро пожаловать, *{0.last_name}* _{1.first_name}_!\nЯ - *{2.first_name}*, бот."
                      .format(message.from_user, message.from_user, bot.get_me()), parse_mode='markdown')
+    bot.send_message(chat_id, "Хочешь подробности?", reply_markup=markup)
 
 
 @bot.message_handler(commands=['sticker'])
@@ -27,7 +37,21 @@ def sticker(message):
 # Если создаём эхо,т.е. что не отправь, он ответит, тогда пишем
 @bot.message_handler(content_types=['text'])
 def echo(message):
-    bot.send_message(message.chat.id, message.text)
+    # bot.send_message(message.chat.id, message.text)
+    chat_id = message.chat.id
+    text = message.text
+    if message.chat.type == 'private':
+        if text == REPLY_LIST[0]:
+            bot.send_message(chat_id, "LOOOOOOOL, ЗАТРАЛЛЕН🤣")
+        elif text == REPLY_LIST[1]:
+            bot.send_message(chat_id, "Больно и хотелось...")
+        elif text == REPLY_LIST[2]:
+            bot.send_message(chat_id, "ЧИВОООООО?")
+        elif text == REPLY_LIST[3]:
+            bot.send_message(chat_id, "Реклама СБП?")
+        else:
+            bot.send_message(chat_id, "ДУПЛО СЕБЕ ОТМЕНИ!")
+
 
 
 # Запускаем бота
